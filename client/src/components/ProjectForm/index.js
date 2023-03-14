e03892cebc56b2101909e4d4012619a926bb75c0
cameliabenavides10 <cameliabenavides@gmail.com>
2023-03-14 12:21:02 -0500

    pushing ians changes

--- client/src/components/ProjectForm/index.js
+++ client/src/components/ProjectForm/index.js
@@ -10,27 +10,29 @@ import Auth from '../../utils/auth';
 const ProjectForm = () => {
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
-
+console.log("LINE 13")
     // const [characterCount, setCharacterCount] = useState(0);
-
+console.log("LINE15")
     const [addProject, { error }] = useMutation(ADD_PROJECT, {
         update(cache, { data: { addProject } }) {
+            console.log("LINE 18")
             try {
                 const { Projects } = cache.readQuery({ query: QUERY_PROJECT });
-
+console.log("LINE20")
                 cache.writeQuery({
                     query: QUERY_PROJECT,
                     data: { Projects: [addProject, ...Projects] },
                 });
             } catch (e) {
                 console.error(e);
+                console.log("LINE 27")
             }
 
             // update me object's cache
             const { me } = cache.readQuery({ query: QUERY_ME });
             cache.writeQuery({
                 query: QUERY_ME,
-                data: { me: { ...me, Projects: [...me.Projects, addProject] } },
+                data: { me: { ...me, projects: [...me.projects, addProject] } },
             });
         },
     });
@@ -39,35 +41,40 @@ const ProjectForm = () => {
         event.preventDefault();
 
         try {
+            console.log("LINE 44")
             const { data } = await addProject({
                 variables: {
                     title,
                     description,
                     projectAuthor: Auth.getProfile().data.username,
+                
                 },
+                
             });
 
+            console.log(title)
             setTitle('');
             setDescription('');
         } catch (err) {
+            console.log("LINE 55")
             console.error(err);
         }
     };
 
-    // const handleChange = (event) => {
-    //     const { name, value } = event.target;
+    const handleChange = (event) => {
+        const { name, value } = event.target;
 
-    //     if (name === 'title' && value.length <= 280) {
-    //         setTitle(value);
-    //         // setCharacterCount(value.length);
-    //     }
+        if (name === 'title' && value.length <= 280) {
+            setTitle(value);
+            // setCharacterCount(value.length);
+        }
 
-    //     if (name === 'description' && value.length <= 280) {
-    //         setTitle(value);
-    //         // setCharacterCount(value.length);
-    //     }
+        if (name === 'description' && value.length <= 280) {
+            setDescription(value);
+            // setCharacterCount(value.length);
+        }
 
-    // };
+    };
 
     return (
         <div>
@@ -80,14 +87,14 @@ const ProjectForm = () => {
                         onSubmit={handleFormSubmit}
                     >
                         <div className="col-12 col-lg-9">
-                            <input name="title" placeholder='add title' className='form-input'></input>
+                            <input name="title" placeholder='add title' value={title} className='form-input' onChange={handleChange}></input>
                             <textarea
                                 name="description"
                                 placeholder="Here's a new Project..."
-                                // value={description}
+                                value={description}
                                 className="form-input w-100"
                                 style={{ lineHeight: '1.5', resize: 'vertical' }}
-                            // onChange={handleChange}
+                            onChange={handleChange}
                             ></textarea>
                         </div>
 
