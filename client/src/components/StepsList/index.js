8135b28b6844edd344192bc784b1bae077c6be55
cameliabenavides10 <cameliabenavides@gmail.com>
2023-03-14 20:52:08 -0500

    rendering steps, trying to fix add step

--- client/src/components/StepsList/index.js
+++ client/src/components/StepsList/index.js
@@ -1,40 +1,54 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
-
 import { ADD_STEP } from '../../utils/mutations';
-
 import Auth from '../../utils/auth';
 
 const StepForm = ({ projectId }) => {
-  const [stepText, setStepText] = useState('');
+  const [addStepText, setAddStepText] = useState({
+        projectId: projectId._id,
+          stepText: "",
+          completed: false
+  });
+  
+  const {stepText, completed} = addStepText
   const [characterCount, setCharacterCount] = useState(0);
 
   const [addStep, { error }] = useMutation(ADD_STEP);
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
-
+console.log("line24")
+console.log(projectId._id)
+console.log(projectId)
+console.log(stepText)
+console.log(completed)
     try {
+      console.log("line 27")
       const { data } = await addStep({
         variables: {
           projectId,
-          stepText,
+          ...addStepText,
         },
+        
       });
-
-      setStepText('');
+      console.log("line33")
+      setAddStepText('');
     } catch (err) {
+      console.log("line36")
       console.error(err);
     }
   };
 
   const handleChange = (event) => {
     const { name, value } = event.target;
-
+    console.log("line42")
     if (name === 'stepText' && value.length <= 280) {
-      setStepText(value);
+      setAddStepText({
+        ...addStepText, [name]:value,
+      });
       setCharacterCount(value.length);
+      
     }
   };
 
@@ -47,7 +61,7 @@ const StepForm = ({ projectId }) => {
           <p
             className={`m-0 ${
               characterCount === 280 || error ? 'text-danger' : ''
-            }`}
+            }` }
           >
             Character Count: {characterCount}/280
             {error && <span className="ml-2">{error.message}</span>}
@@ -68,9 +82,11 @@ const StepForm = ({ projectId }) => {
             </div>
 
             <div className="col-12 col-lg-3">
-              <button className="btn btn-primary btn-block py-3" type="submit">
-                Add Step
-              </button>
+            <div className="modal-footer mt-4">
+                                    <button type="submit" className="btn btn-secondary" data-bs-dismiss="modal" >Close</button>
+                                    <button type="submit" className="btn btn-main"  >Save Step</button>
+                                </div>
+            
             </div>
           </form>
         </>
