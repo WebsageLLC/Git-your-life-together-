46a2a35dfa5b213120cb00d5a624ba766be66b5a
Stephen Totten <s.g.totten@gmail.com>
2023-03-15 19:26:45 -0500

    emails added to footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -13,35 +13,35 @@ const Footer = () => {
                             <h3>Jessica Ortega </h3>
                             <ul>
                                 <li><a href="https://github.com/JessicaIOrtega">GitHub</a></li>
-                                <li><a href="#">Email</a></li>
+                                <li><a href="mailto: jes.ortega7@gmail.com">Email</a></li>
                             </ul>
                         </div>
                         <div class="col-sm-4 col-md-2 item">
                             <h3>Camelia Benavides </h3>
                             <ul>
                                 <li><a href="https://github.com/cameliabenavides10">GitHub</a></li>
-                                <li><a href="#">Email</a></li>
+                                <li><a href="mailto: cameliabenavides@gmail.com">Email</a></li>
                             </ul>
                         </div>
                         <div class="col-sm-4 col-md-2 item">
                             <h3>Michael Escamilla</h3>
                             <ul>
                                 <li><a href="https://github.com/mescamilla1114">GitHub</a></li>
-                                <li><a href="#">Email</a></li>
+                                <li><a href="mailto: mescamilla1114@gmail.com">Email</a></li>
                             </ul>
                         </div>
                         <div class="col-sm-4 col-md-2 item">
                             <h3>Brett Nachman</h3>
                             <ul>
                                 <li><a href="https://github.com/brettnachman">GitHub</a></li>
-                                <li><a href="#">Email</a></li>
+                                <li><a href="mailto: brettnachman@gmail.com">Email</a></li>
                             </ul>
                         </div>
                         <div class="col-sm-4 col-md-2 item">
                             <h3>Stephen Totten</h3>
                             <ul>
                                 <li><a href="https://github.com/Crothos">GitHub</a></li>
-                                <li><a href="#">Email</a></li>
+                                <li><a href="mailto: s.g.totten@gmail.com">Email</a></li>
                             </ul>
                         </div>
 
