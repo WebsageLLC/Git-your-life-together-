fef93f768f884f3b9e46601ab4485578c4ddc2e2
cameliabenavides10 <cameliabenavides@gmail.com>
2023-03-23 13:21:37 -0500

    pullinh

--- client/src/components/StepsList/index.js
+++ client/src/components/StepsList/index.js
@@ -1,29 +1,44 @@
 import React from 'react';
+import { useQuery } from '@apollo/client';
+import { QUERY_PROJECT } from '../../utils/queries';
 
-const StepList = ({ steps = [] }) => {
-  if (!steps.length) {
-    return <h3>No Steps Yet</h3>;
+const StepList = ({ projectId }) => {
+
+  const { loading, data } = useQuery(QUERY_PROJECT, {
+    variables: { projectId },
+})
+
+
+const project = data?.project || {};
+console.log(project)
+
+if (loading) {
+    return <div>Loading...</div>;
+}
+  if (!project.steps.length) {
+    return <h3>No Steps Yet!</h3>;
   }
+
+ 
 
   return (
     <>
       <div className='row ms-5'>
         <h4 className="row ps-5" > Steps</h4>
         <div className="flex-row">
-          {steps &&
-            steps.map((step) => (
-              <div key={step._id} className="row align-items-center ps-5 mt-1 mb-5">
-                <div className='col-6'>
-                  <h5 style={{ color: '#3120E0' }}><strong> {step.stepText}</strong></h5>
+          {project.steps &&
+            project.steps.map((step) => (
+              <div key={step._id} className="col-12 ">
+                <div className=" ps-5 mt-1 mb-5">
+                  <h5 style={{ color: '#3120E0' }}><strong>- {step.stepText}</strong></h5>
+
                   <h5 >
                     <span style={{ fontSize: '0.825rem' }}>{step.createdAt}
                     </span>
                   </h5>
+                  <hr style={{ color: 'coral', width: '50rem' }}></hr>
+
                 </div>
-                <div className="col-6" >
-                  <button className="btn btn-delete m-5 mx-2">Completed</button>
-                </div>
-                <hr style={{ color: 'coral', width: '70%' }}></hr>
               </div>
             ))}
         </div>
